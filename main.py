2233ffdcfaf745c336272c040ff5696ecd5ebe7f
Ivan Gibert <vngbrt@gmail.com>
2023-01-30 17:30:06 +0300

    Edited main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import my_package.functions
-import my_package.constants
+# import my_package.functions
+# import my_package.constants
 
-my_package.functions.greet(my_package.constants.PERSON)
+# my_package.functions.greet(my_package.constants.PERSON)
+
+from my_package.functions import greet
+from my_package.constants import PERSON
+
+greet(PERSON)
