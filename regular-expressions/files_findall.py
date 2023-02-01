fd358b8269b330d03a6178b322cac9c062935f70
Ivan Gibert <vngbrt@gmail.com>
2023-02-01 20:54:49 +0300

    Edited files_findall.py

--- regular-expressions/files_findall.py
+++ regular-expressions/files_findall.py
@@ -1,6 +1,9 @@
 import re
 
 # Open file
-file = open('file.txt', 'r')
+file = open('/home/ivan/education-python/regular-expressions/file.txt', 'r')
 # Feed the file text into findall(); it returns a list of all the found strings
 emails = re.findall(r'[\w.-]+@[\w.-]+', file.read())
+
+for email in emails:
+    print(email)
