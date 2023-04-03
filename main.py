a7d18ed15ce4046ecf43d7256f36c2e18b60e891
Ivan Gibert <vngbrt@gmail.com>
2023-04-03 19:23:38 +0300

    implemented get_square_roots

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 #! usr/bin/env python3
+import math
 
-def get_pow(number_one, power=2):
-    """Return power of number one to number two"""
-    return number_one ** power
 
-print(get_pow(2))
+def get_square_roots(number):
+    """Returns the square roots of a number"""
+    result_list = []
+
+    if number < 0:
+        result_list.append('None')  # No roots
+        return result_list
+
+    elif number == 0:
+        zero = int(math.sqrt(number))  # Zero has only one root - zero
+        result_list.append(zero)
+        return result_list
+
+    else:
+        # Returns two roots. The first is negative, the second is positive
+        positive_root = round(math.sqrt(number), 2)
+        negative_root = -positive_root
+        result_list.extend([negative_root, positive_root])
+        return result_list
+
+
+def get_range():
+    pass
+
+
+print(get_square_roots(9))
