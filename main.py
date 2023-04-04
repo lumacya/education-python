7a3493de52c8f426fc91e486a161021c07187f55
Ivan Gibert <vngbrt@gmail.com>
2023-04-04 19:14:57 +0300

    complete solution

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 #! usr/bin/env python3
-def duplicate(lst):
-    return lst * 2
+def rotate(lst):
+    if len(lst) == 0:
+        return lst
+    else:
+        lst.insert(0, lst.pop())
+        return lst
 
-print(duplicate([1, 2, 3]))
+lst = [1, 2, 3, 4]
+print(rotate(lst))
