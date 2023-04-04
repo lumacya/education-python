25ec9465579bd558b4bd912e33fa978f191fc533
Ivan Gibert <vngbrt@gmail.com>
2023-04-04 20:20:43 +0300

    complete solution

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 #! usr/bin/env python3
-def rotate(lst):
-    if len(lst) == 0:
-        return lst
-    else:
-        lst.insert(0, lst.pop())
-        return lst
+def rotated_left(arg):
+    fisrt_elem = arg[:1]  # if type(arg) is list or tuple => arg[0] is int
+    without_first = arg[1:]
+    result = without_first + fisrt_elem
+    return result  # if type(arg) is list or tuple => TypeError: can only concatenate list (not "int") to list
 
-lst = [1, 2, 3, 4]
-print(rotate(lst))
+def rotated_right(arg):
+    last_elem = arg[-1:]  # if type(arg) is list or tuple => arg[0] is int
+    without_last = arg[:-1]
+    result = last_elem + without_last
+    return result
+
+
+print(rotated_left((1, 2, 3, 4)))
