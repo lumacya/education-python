367fe8cf852aece1296c7a2071f76b25731d5ff8
Ivan Gibert <vngbrt@gmail.com>
2023-04-05 17:33:50 +0300

    complete solution

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 #! usr/bin/env python3
-def rotated_left(arg):
-    fisrt_elem = arg[:1]  # if type(arg) is list or tuple => arg[0] is int
-    without_first = arg[1:]
-    result = without_first + fisrt_elem
-    return result  # if type(arg) is list or tuple => TypeError: can only concatenate list (not "int") to list
+def find_index(value, iter_obj):
+    for index, item in enumerate(iter_obj):
+        if item == value:
+            return index
+        else:
+            continue
 
-def rotated_right(arg):
-    last_elem = arg[-1:]  # if type(arg) is list or tuple => arg[0] is int
-    without_last = arg[:-1]
-    result = last_elem + without_last
-    return result
+    return None
 
-# FIX: Reuse code of one function to other function
-
-print(rotated_left((1, 2, 3, 4)))
+print(find_index(2, [1, 3, 5]))
