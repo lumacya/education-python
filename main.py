566b9ca75a85d98adc70fbf2cfe78c799e186dcc
Ivan Gibert <vngbrt@gmail.com>
2023-04-13 16:20:33 +0300

    complete solution

--- main.py
+++ main.py
@@ -1,24 +1,7 @@
 #! usr/bin/env python3
-from functools import reduce
-from operator import truth, getitem
+def make_module(step=1):
+    return {'inc': lambda x: x + step, 'dec': lambda x: x - step}
 
 
-def keep_truthful(iterable):
-    return list(filter(truth, iterable))
-
-def abs_sum(iterable):
-    return sum(list(map(abs, iterable))) # type: ignore
-
-def walk(dct, path):  #FIX
-    return reduce(getitem, dct, path)
-
-
-dct = {
-    'a': {
-        7: {
-            'b': 42
-        }
-    }
-}
-
-print(walk(dct, ['a', 7, 'b']))
+m = make_module(step=2)
+print(m['inc'] (10))
