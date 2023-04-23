4c6a83bd6c56561be9ba9f06f506a3749722de26
Ivan Gibert <vngbrt@gmail.com>
2023-04-23 16:43:11 +0300

    recursion matreshka

--- main.py
+++ main.py
@@ -1,33 +1,11 @@
 #! usr/bin/env python3
-def make_decart_point(x, y):
-    point = {
-        'x': x,
-        'y': y
-    }
-
-    return point
-
-
-def make_segment(start_point, end_point):
-    segment = {
-        'start_point': start_point,
-        'end_point': end_point
-    }
-
-    return segment
+def matreshka(n):
+    if n == 1:
+        print('Matreshechka')
+    else:
+        print(f'top of matreshka {n}')
+        matreshka(n - 1)
+        print(f'bottom of matreshka {n}')
 
 
-def get_mid_point_of_segment(segment):
-    # Can I optimize this code?
-    mid_x = (segment['start_point']['x'] + segment['end_point']['x']) / 2
-    mid_y = (segment['start_point']['y'] + segment['end_point']['y']) / 2
-    mid_point = {
-        'x': mid_x,
-        'y': mid_y
-    }
-
-    return mid_point
-
-
-segment = make_segment(make_decart_point(3, 2), make_decart_point(0, 0))
-print(get_mid_point_of_segment(segment))
+matreshka(5)
