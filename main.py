9b4bdad9174538328fa9c8734d1a9250bc7259d6
Ivan Gibert <vngbrt@gmail.com>
2023-04-25 19:40:10 +0300

    files

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 #! usr/bin/env python3
-input_file = open('input.txt', 'r')
-output_file = open('output.txt', 'w')
-
-for i, line in enumerate(input_file, 1):
-    output_file.write(f'{i}) {line}')
-
-input_file.close()
-output_file.close()
+with open('input.txt', 'r') as input_file:
+    with open('output.txt', 'w') as output_file:
+        for i, line in enumerate(input_file, 1):
+            output_file.write(f'{i}) {line}')
