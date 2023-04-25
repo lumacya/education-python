da61f367bccd6c28547f723ee280c666cf98610f
Ivan Gibert <vngbrt@gmail.com>
2023-04-25 19:16:03 +0300

    working with files

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 #! usr/bin/env python3
-f = open('foo.txt', 'w')
-f.write('Hello, World!\nI am Ivan!')
-f.close()
+input_file = open('input.txt', 'r')
+output_file = open('output.txt', 'w')
+
+for i, line in enumerate(input_file, 1):
+    output_file.write(f'{i}) {line}')
+
+input_file.close()
+output_file.close()
