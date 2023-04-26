2baa530419aad601be41ba21eac71d1207fd3110
Ivan Gibert <vngbrt@gmail.com>
2023-04-26 15:06:46 +0300

    delete io files

--- main.py
+++ main.py
@@ -1,5 +1,2 @@
 #! usr/bin/env python3
-with open('input.txt', 'r') as input_file:
-    with open('output.txt', 'w') as output_file:
-        for i, line in enumerate(input_file, 1):
-            output_file.write(f'{i}) {line}')
+
