c94e9cd712e0238a9c126d3ffe7f16bdb3a42a99
Ivan Gibert <vngbrt@gmail.com>
2023-05-16 16:30:25 +0300

    oop property

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 #! usr/bin/env python3
+class Person:
+    def __init__(self, name, surname) -> None:
+        self.name = name
+        self.surname = surname
 
+    @property
+    def full_name(self):
+        return f'{self.name} {self.surname}'
+
+    @full_name.setter
+    def full_name(self, new):
+        self.name, self.surname = new.split(' ')
+
+
+tom = Person('Thomas', 'Shelby')
+tom.full_name = 'Alice Cooper'
+print(tom.full_name)
